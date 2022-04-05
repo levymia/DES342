d803b162c176c390c12d6ef2a2daf1901155b6a2
levymia <levymia@pdx.edu>
2022-04-05 11:16:10 -0700

    kitty slides yay

--- jquery-play/script.js
+++ jquery-play/script.js
@@ -1,3 +1,28 @@
 // I'm some javascript! 
 
-$('body').css('background', 'pink')
+// $('body').css('background', 'pink')
+
+$('h1').click(
+    function() {
+        $('body').toggleClass('purple')
+    })
+
+// let rainbow = 0
+// let colorVal = `hsl(${rainbow},100,100)`
+
+// $('.button').click(function(){
+//     rainbow + 30
+//     $('body').css('background', `${colorVal}`)
+// })
+
+$('.one').click(function(){
+        $('.kitties').attr('src','http://placekitten.com/400/401')
+    })
+
+$('.two').click(function(){
+        $('.kitties').attr('src','http://placekitten.com/401/401')
+    })
+
+$('.three').click(function(){
+        $('.kitties').attr('src','http://placekitten.com/402/401')
+     })
