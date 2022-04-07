da3581e09b7273903ecaf5468db276832e57b15c
levymia <levymia@pdx.edu>
2022-04-06 17:22:35 -0700

    slow progress

---
+++ homeworks/hw2/images/script.js
@@ -0,0 +1,47 @@
+$('.two').click(
+    function(){
+        $('robin-rounds').attr('scr', 'img/robin2.png')
+    }
+)
+
+$('.three').click(
+    function(){
+        $('.robin-rounds').attr('scr', 'images/robin3.png')
+    }
+)
+
+$('.four').click(
+    function(){
+        $('.robin-rounds').attr('scr', 'images/robin4.png')
+    }
+)
+
+$('.five').click(
+    function(){
+        $('.robin-rounds').attr('scr', 'images/robin5.png')
+    }
+)
+
+$('.six').click(
+    function(){
+        $('.robin-rounds').attr('scr', 'images/robin6.png')
+    }
+)
+
+$('.seven').click(
+    function(){
+        $('.robin-rounds').attr('scr', 'images/robin7.png')
+    }
+)
+
+$('.eight').click(
+    function(){
+        $('.robin-rounds').attr('scr', 'images/robin8.png')
+    }
+)
+
+$('.nine').click(
+    function(){
+        $('.robin-rounds').attr('scr', 'images/robin9.png')
+    }
+)
