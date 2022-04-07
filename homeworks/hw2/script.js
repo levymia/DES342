682b1d1899dcddb1fbd1fe25de5f3919f6dad688
levymia <levymia@pdx.edu>
2022-04-06 20:31:41 -0700

    i did it!!!!!

--- homeworks/hw2/script.js
+++ homeworks/hw2/script.js
@@ -1,3 +1,35 @@
 $('.one').click(function (){
-    $('animal-pic').attr('src', 'images/robin2.png')
+    $('.comic-pic').attr('src', 'images/robin1.png')
+})
+
+$('.two').click(function (){
+    $('.comic-pic').attr('src', 'images/robin2.png')
+})
+
+$('.three').click(function (){
+    $('.comic-pic').attr('src', 'images/robin3.png')
+})
+
+$('.four').click(function (){
+    $('.comic-pic').attr('src', 'images/robin4.png')
+})
+
+$('.five').click(function (){
+    $('.comic-pic').attr('src', 'images/robin5.png')
+})
+
+$('.six').click(function (){
+    $('.comic-pic').attr('src', 'images/robin6.png')
+})
+
+$('.seven').click(function (){
+    $('.comic-pic').attr('src', 'images/robin7.png')
+})
+
+$('.eight').click(function (){
+    $('.comic-pic').attr('src', 'images/robin8.png')
+})
+
+$('.nine').click(function (){
+    $('.comic-pic').attr('src', 'images/robin9.png')
 })
